5bfd37399adfa3e4f3da70721d810d1589dbf40b
AaronBullard <abullard@theviinetwork.com>
2015-02-11 15:00:58 -0500

    added unprocessable entity exception

---
+++ src/Aaronbullard/Exceptions/UnprocessableEntityException.php
@@ -0,0 +1,24 @@
+<?php namespace Aaronbullard\Exceptions;
+
+class UnprocessableEntityException extends HttpException {
+	const HTTP_CODE = 422;
+	const DESCRIPTION = "The 422 (Unprocessable Entity) status code means the server understands the 
+		content type of the request entity (hence a 415(Unsupported Media Type) status code is inappropriate), 
+		and the syntax of the request entity is correct (thus a 400 (Bad Request) status code is inappropriate) 
+		but was unable to process the contained instructions. For example, this error condition may occur if an XML 
+		request body contains well-formed (i.e., syntactically correct), but semantically erroneous, XML instructions.";
+
+	protected $errors;
+
+	public function setErrors(array $errors)
+	{
+		$this->errors = $errors;
+
+		return $this;
+	}
+	
+	public function getErrors()
+	{
+		return $this->errors;
+	}
+}
